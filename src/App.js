3ab5829b93bdde8ee69f8c34ed0b3aa7c02d2e1b
Dmytro Mytysh <mitishdima@yandex.ua>
2019-04-22 22:54:02 +0200

    working with redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
 
 class App extends Component {
+  addTask() {
+    console.log("testing adding task?")
+    
+    this.props.onAddTask(this.taskInput.value);
+    this.taskInput.value = '';
+  }
+  deleteTask(id){
+    const tasks = this.state.tasks.filter(tasks => {
+      return tasks.id !== id
+    });
+    // this.props.onDeleteTask(this.taskInput.value);
+    this.setState({
+      tasks
+    });
+  }
+
+
   render() {
+    // console.log(this.props.testStore);
     return (
-      <div >
-       
-      </div>
+       <div>
+          <input type="text" ref={(input) => { this.taskInput = input; }} />
+          <button onClick={this.addTask.bind(this)}>Add task</button>
+          <button onClick={this.deleteTask.bind(this)}>Delete</button>
+              <ul>
+              {this.props.tasks.map((task, index) =>
+              <li key={index}>{task}</li>
+              )}
+              </ul>
+        </div>
     );
   }
 }
 
-export default App;
+export default connect(
+  state => ({
+    tasks: state.tasks
+  }),
+  dispatch => ({
+    onAddTask: (taskName) => {
+      dispatch({ type: 'ADD_TASK', payload: taskName })
+    },
+    // onDeleteTask: (taskId) =>{
+    //   dispatch({type: 'DELETE_TASK', payload: taskId})
+    // }
+  })
+)(App);
+
--- src/index.js
+++ src/index.js
@@ -3,10 +3,38 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const initialState = {
+tasks: [
+    "Testing first state"
+]
+};
+
+
+
+// this is my reduser( czysta funkcja )
+function listToDo(state = initialState, action) {
+    if (action.type === 'ADD_TASK') {
+        return {
+          ...state,
+          tasks: [...state.tasks, action.payload]
+    
+    };
+      }else if (action.type === 'DELETE_TASK') {
+      return state;
+    }   
+    return state;
+    }
+//   dodalem devtools w mozila
+    const store = createStore(listToDo, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(<Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'));
+
+
 serviceWorker.unregister();
+
