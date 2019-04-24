fd345ef0a3bdaf9a73ec95996b0abba2980482e2
Dmytro Mytysh <mitishdima@yandex.ua>
2019-04-24 20:54:55 +0200

    hard version of toDoList

--- src/index.js
+++ src/index.js
@@ -1,40 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux'
+import React from 'react'
+import { render } from 'react-dom'
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './components/App'
+import rootReducer from './reducers'
 
+const store = createStore(rootReducer)
 
-const initialState = {
-tasks: [
-    "Testing first state"
-]
-};
-
-
-
-// this is my reduser( czysta funkcja )
-function listToDo(state = initialState, action) {
-    if (action.type === 'ADD_TASK') {
-        return {
-          ...state,
-          tasks: [...state.tasks, action.payload]
-    
-    };
-      }else if (action.type === 'DELETE_TASK') {
-      return state;
-    }   
-    return state;
-    }
-//   dodalem devtools w mozila
-    const store = createStore(listToDo, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
-ReactDOM.render(<Provider store={store}>
+render(
+  <Provider store={store}>
     <App />
-  </Provider>, document.getElementById('root'));
-
-
-serviceWorker.unregister();
-
+  </Provider>,
+  document.getElementById('root')
+)
